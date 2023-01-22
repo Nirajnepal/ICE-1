c5d6c3e3f0a82159ae985faa6ce26ab2d00e6ae1
Niraj <mathersnepal@gmail.com>
2023-01-22 17:18:12 -0500

    Added ocean and player to the game

---
+++ ICE Lab 1/Core/GameProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  GameProtocol.swift
+//  ICE Lab 1
+//
+//  Created by Niraj Nepal on 22/01/2023.
+//
+
+protocol GameProtocol
+{
+    func Start()
+    
+    func Update()
+    
+    func CheckBounds()
+    
+    func Reset()
+}
+
